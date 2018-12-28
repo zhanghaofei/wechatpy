257ea4bf1c7ed7b1e925ff99b6a58a8e445a0bbc
zhanghaofei <zhanghaofei@tbkt.cn>
2018-12-28 09:14:06 +0800

    version my-v0.0.9

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='weixinpy',
-    version='0.0.8',
+    version='0.0.9',
     author='zhanghaofei',
     author_email='1042697116@qq.com',
     url='https://github.com/zhanghaofei/weixinpy',
